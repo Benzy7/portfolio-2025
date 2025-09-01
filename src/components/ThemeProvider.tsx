b936047435c47a1b303be6171b0e5b37c59ed903
khoubaib <khoubaib.bnz@gmail.com>
2025-09-01 12:16:12 +0100

    hotfix2

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,11 +1,8 @@
-'use client';
+'use client' // if using Next 13 app directory
 
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ThemeProvider as NextThemeProvider } from 'next-themes'
+import type { ThemeProviderProps as NextThemeProviderProps } from 'next-themes'
 
-interface ThemeProviderProps {
-  children: React.ReactNode;
-}
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+export const ThemeProvider = (props: NextThemeProviderProps) => {
+  return <NextThemeProvider {...props} />
 }
